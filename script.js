ad34f946089b2f94fdd34b8fdcd0ad22ecba15dd
sidhaggarwal <sidh.aggarwal30@gmail.com>
2026-09-03 22:06:52 -0400

    Edited script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const video = document.getElementById("webcam");
 
 async function setupWebcam() {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+        const stream = await navigator.mediaDevices.getUserMedia({video: {
+            width: {exact: 800},
+            height: {exact:600}
+        }, audio: false});
         video.srcObject = stream;
     } catch (err) {
-           console.error("Error accessing the webcam: ", error);
-        alert("Could not access camera. Please check browser permissions.");
+           console.error("Error accessing the webcam: ", err);
+        
+           alert("Could not access camera at 800x600 resolution. Please check browser permissions or camera hardware.");
     }
 }
 setupWebcam();
